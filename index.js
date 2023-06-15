a9cda2e9ea094c053674c47d016c252c21be3415
Christopher Gold <crgold@asu.edu>
2023-06-15 15:30:22 +0800

    Tweak lesson code to make challenge easier

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Import Solana web3 functinalities
+// Import Solana web3 functionalities
 const {
     Connection,
     PublicKey,
@@ -7,39 +7,14 @@ const {
     LAMPORTS_PER_SOL,
     Transaction,
     SystemProgram,
-    sendAndConfirmRawTransaction,
     sendAndConfirmTransaction
 } = require("@solana/web3.js");
-
-
-// Making a keypair and getting the private key
-const newPair = Keypair.generate();
-console.log(newPair);
-
-// paste your secret that is logged here
-const DEMO_FROM_SECRET_KEY = new Uint8Array(
-  // paste your secret key array here
-    [
-        160,  20, 189, 212, 129, 188, 171, 124,  20, 179,  80,
-         27, 166,  17, 179, 198, 234,  36, 113,  87,   0,  46,
-        186, 250, 152, 137, 244,  15,  86, 127,  77,  97, 170,
-         44,  57, 126, 115, 253,  11,  60,  90,  36, 135, 177,
-        185, 231,  46, 155,  62, 164, 128, 225, 101,  79,  69,
-        101, 154,  24,  58, 214, 219, 238, 149,  86
-      ]            
-);
 
 const transferSol = async() => {
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 
-    // Get Keypair from Secret Key
-    var from = Keypair.fromSecretKey(DEMO_FROM_SECRET_KEY);
-
-    // Other things to try: 
-    // 1) Form array from userSecretKey
-    // const from = Keypair.fromSecretKey(Uint8Array.from(userSecretKey));
-    // 2) Make a new Keypair (starts with 0 SOL)
-    // const from = Keypair.generate();
+    // Generate a new keypair
+    const from = Keypair.generate();
 
     // Generate another Keypair (account we'll be sending to)
     const to = Keypair.generate();
